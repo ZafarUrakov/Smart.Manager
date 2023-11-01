39c1bab02390cf30acfcdd1e1e737da8443e99e9
Zafar <icom@MACKBOOK>
2023-11-01 10:43:12 +0300

    DATA: Create Model

---
+++ SmartManager/Models/ExternalStudents/ExternalStudent.cs
@@ -0,0 +1,23 @@
+﻿//===========================
+// Copyright (c) Tarteeb LLC
+// Managre quickly and easy
+//===========================
+
+using SmartManager.Models.Groups;
+using SmartManager.Models.Students;
+using System;
+
+namespace SmartManager.Models.ExternalStudents
+{
+    public class ExternalStudent
+    {
+        public Guid Id { get; set; }
+        public string GivenName { get; set; }
+        public string Surname { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTimeOffset BirthDate { get; set; }
+        public Gender Gender { get; set; }
+        public Guid GroupId { get; set; }
+        public Group Group { get; set; }
+    }
+}
