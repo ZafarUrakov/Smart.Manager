ff9eebee7c61df9fd1accc8bae2877b7e827af66
Zafar <icom@MACKBOOK>
2023-11-01 22:20:42 +0300

    POC: Models Migrate & Crud

---
+++ SmartManager/Migrations/20231101143727_AddStudentsTable.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStudentsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Attendance_Student_StudentId",
+                table: "Attendance");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Payment_Student_StudentId",
+                table: "Payment");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Student_Groups_GroupId",
+                table: "Student");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Student",
+                table: "Student");
+
+            migrationBuilder.RenameTable(
+                name: "Student",
+                newName: "Students");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Student_GroupId",
+                table: "Students",
+                newName: "IX_Students_GroupId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Students",
+                table: "Students",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Attendance_Students_StudentId",
+                table: "Attendance",
+                column: "StudentId",
+                principalTable: "Students",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Payment_Students_StudentId",
+                table: "Payment",
+                column: "StudentId",
+                principalTable: "Students",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Students_Groups_GroupId",
+                table: "Students",
+                column: "GroupId",
+                principalTable: "Groups",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Attendance_Students_StudentId",
+                table: "Attendance");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Payment_Students_StudentId",
+                table: "Payment");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Students_Groups_GroupId",
+                table: "Students");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Students",
+                table: "Students");
+
+            migrationBuilder.RenameTable(
+                name: "Students",
+                newName: "Student");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Students_GroupId",
+                table: "Student",
+                newName: "IX_Student_GroupId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Student",
+                table: "Student",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Attendance_Student_StudentId",
+                table: "Attendance",
+                column: "StudentId",
+                principalTable: "Student",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Payment_Student_StudentId",
+                table: "Payment",
+                column: "StudentId",
+                principalTable: "Student",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Student_Groups_GroupId",
+                table: "Student",
+                column: "GroupId",
+                principalTable: "Groups",
+                principalColumn: "Id");
+        }
+    }
+}
---
+++ SmartManager/Services/Foundations/Students/IStudentService.cs
@@ -0,0 +1,21 @@
+﻿//===========================
+// Copyright (c) Tarteeb LLC
+// Managre quickly and easy
+//===========================
+
+using System.Linq;
+using System.Threading.Tasks;
+using System;
+using SmartManager.Models.Students;
+
+namespace SmartManager.Services.Foundations.Students
+{
+    public interface IStudentService
+    {
+        ValueTask<Student> AddStudentAsync(Student student);
+        ValueTask<Student> RetrieveStudentByIdAsync(Guid studentid);
+        IQueryable<Student> RetrieveAllStudents();
+        ValueTask<Student> ModifyStudentAsync(Student student);
+        ValueTask<Student> RemoveStudentAsync(Guid studentid);
+    }
+}
