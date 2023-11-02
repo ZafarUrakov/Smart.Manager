65728e0d484b0031ee127a4ce563da02343e8c88
Jamshidbek Tursunboev <jamshidbektursunboev@gmail.com>
2023-11-02 23:55:09 +0900

    POC: Create Payment

---
+++ SmartManager/Controllers/GroupController.cs
@@ -0,0 +1,29 @@
+﻿//===========================
+// Copyright (c) Tarteeb LLC
+// Managre quickly and easy
+//===========================
+
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using SmartManager.Models.Groups;
+using SmartManager.Services.Processings.Groups;
+
+namespace SmartManager.Controllers
+{
+    public class GroupController : Controller
+    {
+        private readonly IGroupProcessingService groupProcessingService;
+
+        public GroupController(IGroupProcessingService groupProcessingService)
+        {
+            this.groupProcessingService = groupProcessingService;
+        }
+
+        public ActionResult GetAllGroups()
+        {
+            IQueryable<Group> groups = this.groupProcessingService.RetrieveAllGroups();
+
+            return View(groups);
+        }
+    }
+}
