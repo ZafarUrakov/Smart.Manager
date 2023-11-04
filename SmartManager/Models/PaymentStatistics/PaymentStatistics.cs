5e3922c0fbdcfc14e4ec30ffe63edbe70735aebb
Zafar <icom@MACKBOOK>
2023-11-04 17:33:24 +0300

    CODE RUB: Fix Bugs

---
+++ SmartManager/Models/PaymentStatistics/PaymentStatistics.cs
@@ -0,0 +1,19 @@
+﻿//===========================
+// Copyright (c) Tarteeb LLC
+// Managre quickly and easy
+//===========================
+
+using SmartManager.Models.Groups;
+using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace SmartManager.Models.PaymentStatistics
+{
+    public class PaymentStatistics
+    {
+        Guid Id { get; set; }
+        public decimal PaidPercentage { get; set; }
+        public decimal NotPaidPercentage { get; set; }
+        public Group Group { get; set; }
+    }
+}
